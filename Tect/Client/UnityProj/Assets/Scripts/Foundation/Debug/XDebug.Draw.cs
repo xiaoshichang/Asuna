7a8cecc0599ddedce8f209b4a5e47a522b95bc06
xiaoshichang <511680777@qq.com>
2022-12-01 14:20:04 +0800

    Debug drawline

---
+++ Tect/Client/UnityProj/Assets/Scripts/Foundation/Debug/XDebug.Draw.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace AsunaClient.Foundation
+{
+    public static partial class XDebug
+    {
+
+        #region Draw Line
+        public static void DrawLine(Vector3 start, Vector3 end, Color color)
+        {
+            Debug.DrawLine(start, end, color);
+        }
+
+        public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration)
+        {
+            Debug.DrawLine(start, end, color, duration);
+        }
+
+        public static void DrawLine(Vector3 start, Vector3 end, Color color, float duration, bool depthTest)
+        {
+            Debug.DrawLine(start, end, color, duration, depthTest);
+        }
+        #endregion
+
+    }
+}
