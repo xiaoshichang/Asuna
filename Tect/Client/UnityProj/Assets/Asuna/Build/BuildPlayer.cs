bb527cbac1f0d5ffb4fec1f762dadeaf366add55
xiaoshichang <511680777@qq.com>
2023-02-05 16:55:23 +0800

    build menu

---
+++ Tect/Client/UnityProj/Assets/Asuna/Build/BuildPlayer.cs
@@ -0,0 +1,43 @@
+﻿using Asuna.Utils;
+using UnityEditor;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+
+namespace Asuna.Build
+{
+    public static class BuildPlayerPipeline
+    {
+        [MenuItem("Asuna/Build/BuildPlayer - Windows")]
+        public static void BuildPlayerWindows()
+        {
+            BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+            buildPlayerOptions.scenes = new[] { "Assets/Asuna/Res/Scenes/Main.unity"};
+            buildPlayerOptions.locationPathName = "windows64Build";
+            buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+            buildPlayerOptions.options = BuildOptions.None;
+            BuildPlayer(buildPlayerOptions);
+        }
+
+        private static void BuildPlayer(BuildPlayerOptions buildPlayerOptions)
+        {
+            BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+            BuildSummary summary = report.summary;
+            if (summary.result == BuildResult.Succeeded)
+            {
+                ADebug.Info("Build Succeeded!");
+            }
+            else if (summary.result == BuildResult.Failed)
+            {
+                ADebug.Error("Build Failed");
+            }
+            else if (summary.result == BuildResult.Cancelled)
+            {
+                ADebug.Warning("Build Cancelled");
+            }
+            else
+            {
+                ADebug.Error("Build Result Unknown");
+            }
+        }
+    }
+}
--- Tect/Client/UnityProj/Assets/Asuna/Check/CodeAnalyze/CodeAnalyze.cs
+++ Tect/Client/UnityProj/Assets/Asuna/Check/CodeAnalyze/CodeAnalyze.cs
@@ -5,7 +5,7 @@ namespace Asuna.Check
 {
     public static class CodeSpecification
     {
-        [MenuItem("Asuna/Check/CodeSpecification")]
+        [MenuItem("Asuna/Check/CodeAnalyze")]
         public static void StartCodeSpecificationCheckPipeline()
         {
             var pipeline = new CodeSpecificationCheckPipeline();
